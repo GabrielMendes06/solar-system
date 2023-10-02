0fa0da759fc85dea53d39ca889aca2d8fff99543
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2023-10-02 20:56:56 -0300

    update 2.3

--- src/components/Footer/FinalPage.tsx
+++ src/components/Footer/FinalPage.tsx
@@ -4,12 +4,12 @@ import './css/footer.css'
 
 const FinalPage: React.FC = () => {
     return(
-        <div className="container-footer m-auto bg-black">
+        <div className="container m-auto bg-black">
                 <div className="row border-bottom">
                     <div className="col-sm flex p-3">
                         <ApresentationMark />
                     </div>
-                    <div className="col-sm flex justify-content-end p-3">
+                    <div className="col-sm flex justify-content-center p-3">
                         <i className="bi bi-linkedin mx-3 text-light size"></i>
                         <i className="bi bi-github mx-3 text-light size"></i>
                         <i className="bi bi-whatsapp mx-3 text-light size"></i>
